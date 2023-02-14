72570983418dbd5f8f870ce74ee3675d263bce06
Changwan Jun <wan2land@gmail.com>
2023-02-15 01:56:31 +0900

    fix: knob action

--- src/components/InputKnob.svelte
+++ src/components/InputKnob.svelte
@@ -1,24 +1,13 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
   export let value: number | null = null;
 
   export let min: number;
   export let max: number;
   export let step: number | null = null;
 
-  export let controlElem: HTMLDivElement | null = null;
-  export let controlSize = 0;
+  export let knobElem: HTMLDivElement | null = null;
   export let controlDashArray = '';
   export let controlDashOffset = 0;
-
-  onMount(() => {
-    const rect = controlElem?.getBoundingClientRect();
-    if (!rect) {
-      return;
-    }
-    controlSize = Math.min(rect.width, rect.height);
-  });
 
   $: {
     setValue(value);
@@ -45,6 +34,13 @@
     controlDashArray = `${D * ratio} ${D * (1 - ratio)}`;
   }
 
+  function setPoint(clientX: number, clientY: number) {
+    const { left, top, width, height } = knobElem!.getBoundingClientRect();
+    const x = left + width / 2 - clientX;
+    const y = top + height / 2 - clientY;
+    value = (Math.atan2(-x, y) / (2 * Math.PI) + 0.5) * (max - min) + min;
+  }
+
   function onWheel(e: WheelEvent) {
     if (e.deltaY) {
       value = (value ?? min) + (e.deltaY > 0 ? 1 : -1) * (step ?? 1);
@@ -57,14 +53,12 @@
   }
 
   function onMouseMove(e: MouseEvent) {
-    const { left, top, width, height } = controlElem!.getBoundingClientRect();
-    const x = left + width / 2 - e.clientX;
-    const y = top + height / 2 - e.clientY;
-    value = (Math.atan2(-x, y) / (2 * Math.PI) + 0.5) * (max - min) + min;
+    setPoint(e.clientX, e.clientY);
   }
 
   function onMouseUp(e: MouseEvent) {
     e.preventDefault();
+    setPoint(e.clientX, e.clientY);
     window.removeEventListener('mousemove', onMouseMove);
     window.removeEventListener('mouseup', onMouseUp);
   }
@@ -76,55 +70,44 @@
 
   function onTouchMove(e: TouchEvent) {
     if (e.touches.length == 1) {
-      const { left, top, width, height } = controlElem!.getBoundingClientRect();
-      const x = left + width / 2 - e.touches[0].clientX;
-      const y = top + height / 2 - e.touches[0].clientY;
-      value = (Math.atan2(-x, y) / (2 * Math.PI) + 0.5) * (max - min) + min;
+      setPoint(e.touches[0].clientX, e.touches[0].clientY);
     }
   }
 
   function onTouchEnd(e: TouchEvent) {
     e.preventDefault();
+    setPoint(e.touches[0].clientX, e.touches[0].clientY);
     window.removeEventListener('touchmove', onTouchMove);
     window.removeEventListener('touchend', onTouchEnd);
   }
 </script>
 
-<div class="input-knob">
-  <div
-    class="input-knob_controlElem"
-    on:wheel|preventDefault|stopPropagation={onWheel}
-    on:mousedown|preventDefault={onMouseDown}
-    on:touchstart|preventDefault={onTouchStart}
-    bind:this={controlElem}
-  >
-    {#if controlSize > 0}
-      <svg
-        width={controlSize}
-        height={controlSize}
-        class="stroke-current text-blue"
-        viewBox="0 0 100 100"
-      >
-        <circle
-          cx="50"
-          cy="50"
-          r="48.5"
-          stroke-width="3"
-          fill="transparent"
-          transform="rotate(90 50 50)"
-          stroke-dasharray={controlDashArray}
-          stroke-dashoffset={controlDashOffset}
-        />
-      </svg>
-    {/if}
-  </div>
+<div
+  class="input-knob"
+  bind:this={knobElem}
+  on:wheel|preventDefault|stopPropagation={onWheel}
+  on:mousedown|preventDefault={onMouseDown}
+  on:touchstart|preventDefault={onTouchStart}
+>
+  <svg width="100" height="100" class="input-knob__control" viewBox="0 0 100 100">
+    <circle
+      cx="50"
+      cy="50"
+      r="48.5"
+      stroke-width="3"
+      fill="transparent"
+      transform="rotate(90 50 50)"
+      stroke-dasharray={controlDashArray}
+      stroke-dashoffset={controlDashOffset}
+    />
+  </svg>
 </div>
 
 <style>
   .input-knob {
     @apply aspect-square relative p-4;
   }
-  .input-knob_controlElem {
-    @apply w-full h-full;
+  .input-knob__control {
+    @apply w-full h-full stroke-current text-blue;
   }
 </style>
